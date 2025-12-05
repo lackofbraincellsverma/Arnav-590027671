964d6341e1ceff19bb3f6a9a211cbdd2733351b3
Arnav Verma <arnav29.verma@gmail.com>
2025-12-05 22:22:58 +0530

    Create d19q1.c

---
+++ d19q1.c
@@ -0,0 +1,36 @@
+Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y;
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (b != 0) {     
+        int t = b;
+        b = a % b;
+        a = t;
+    }
+
+    int gcd = a;
+    int lcm = (x * y) / gcd;
+
+    printf("%d", lcm);
+    return 0;
+}

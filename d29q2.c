95d47cc591543d73422c25b26ae0392c8c8b395c
Arnav Verma <arnav29.verma@gmail.com>
2025-12-05 22:49:27 +0530

    Create d29q2.c

---
+++ d29q2.c
@@ -0,0 +1,37 @@
+Q58: Find the maximum and minimum element in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10
+
+*/
+  #include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    int max = a[0], min = a[0];
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) max = a[i];
+        if (a[i] < min) min = a[i];
+    }
+
+    printf("Max=%d, Min=%d", max, min);
+    return 0;
+}
